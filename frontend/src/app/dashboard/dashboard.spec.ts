f34461e81fb8d52a801fd512b1935b491f45b2a7
Swati Singh <swati.singh@scryai.com>
2026-07-16 11:28:44 +0530

    frontend testing

--- frontend/src/app/dashboard/dashboard.spec.ts
+++ frontend/src/app/dashboard/dashboard.spec.ts
@@ -1,22 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Dashboard } from './dashboard';
+import { provideRouter } from '@angular/router';
+import { of } from 'rxjs';
+
+import { SensexService } from '../services/sensex.service';
+import { SocketService } from '../services/socket';
 
 describe('Dashboard', () => {
   let component: Dashboard;
   let fixture: ComponentFixture<Dashboard>;
 
   beforeEach(async () => {
+
+    const sensexServiceMock = {
+      getSensexData: () =>
+        of({
+          data: [],
+          totalCount: 0,
+          page: 1,
+          limit: 30
+        }),
+      addSensexRecord: () => of({}),
+      getMonthlyAverage: () => of([])
+    };
+
+    const socketServiceMock = {
+      getSocket: () => ({
+        on: () => {}
+      })
+    };
+
     await TestBed.configureTestingModule({
       imports: [Dashboard],
+      providers: [
+        provideRouter([]),
+        { provide: SensexService, useValue: sensexServiceMock },
+        { provide: SocketService, useValue: socketServiceMock }
+      ]
     }).compileComponents();
+
+    localStorage.setItem('jwt', 'dummy-token');
 
     fixture = TestBed.createComponent(Dashboard);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
